9a933ac2f72143571cb175f1a800dab53f51d5aa
NaveenDJ <NaveenDJ@host.docker.internal>
2022-12-28 05:27:19 +0530

    added echo statements

--- deploy-istio.sh
+++ deploy-istio.sh
@@ -1,5 +1,7 @@
 curl -L https://istio.io/downloadIstio | sh -
 cd istio-1.16.1
 export PATH=$PWD/bin:$PATH
+echo "Path is set"
+echo $PATH
 kubectl create namespace istio-system
 istioctl install --set profile=demo

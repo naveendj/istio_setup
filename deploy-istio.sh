ceb9f2c05725ec05447eba53a61f2df55842151e
NaveenDJ <NaveenDJ@host.docker.internal>
2022-12-30 21:57:19 +0530

    some changes

--- deploy-istio.sh
+++ deploy-istio.sh
@@ -3,6 +3,3 @@ cd istio-1.16.1
 export PATH=$PATH:$PWD/bin
 echo "Path is set"
 echo $PATH
-kubectl create namespace istio-system
-echo "istio-system namespace created"
-istioctl install --set profile=demo
